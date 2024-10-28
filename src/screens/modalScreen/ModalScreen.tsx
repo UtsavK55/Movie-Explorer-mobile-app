284e5471368e263513c598b8c39c229256e258b9
UtsavK55 <utsav@gkmit.co>
2024-10-28 14:50:49 +0530

    feat: add confirmation modal

---
+++ src/screens/modalScreen/ModalScreen.tsx
@@ -0,0 +1,12 @@
+import BaseContainer from '@components/baseContainer';
+import ConfirmationModal from '@components/confirmationModal';
+
+const ModalScreen = () => {
+  return (
+    <BaseContainer>
+      <ConfirmationModal />
+    </BaseContainer>
+  );
+};
+
+export default ModalScreen;
