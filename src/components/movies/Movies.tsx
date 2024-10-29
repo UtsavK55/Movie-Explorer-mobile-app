36f2703e3a080902e0d8e62b146452784b2fcd6a
UtsavK55 <utsav@gkmit.co>
2024-10-29 11:04:12 +0530

    feat: add-pagination

--- src/components/movies/Movies.tsx
+++ src/components/movies/Movies.tsx
@@ -4,32 +4,22 @@ import {ScrollView} from 'react-native';
 import Carousel from '@components/carousel';
 import HorizontalMovieScroll from '@components/horizontalMovieScroll';
 import Loader from '@components/loader';
+
 import {getAllMovies} from '@helpers/helper';
+import {discover} from '@network/apiUrls';
 import {colors} from '@theme/themes';
 
 import {styles} from './styles';
 
 const Movies = () => {
+  
   const [isLoading, setIsLoading] = useState(false);
-
   const [carouselData, setCarouselData] = useState([]);
-  const [popularData, setPopularData] = useState([]);
-  const [oldData, setOldData] = useState([]);
-  const [mostViewedData, setMostViewedData] = useState([]);
 
   const getData = async () => {
     setIsLoading(true);
     const carouselMovies = await getAllMovies(1, 'revenue.desc');
     setCarouselData(carouselMovies);
-
-    const popularMovies = await getAllMovies(1, 'popularity.desc');
-    setPopularData(popularMovies);
-
-    const oldMovies = await getAllMovies(1, 'primary_release_date.asc');
-    setOldData(oldMovies);
-
-    const mostViewedMovies = await getAllMovies(1, 'vote_count.desc');
-    setMostViewedData(mostViewedMovies);
     setIsLoading(false);
   };
 
@@ -40,15 +30,25 @@ const Movies = () => {
   if (isLoading) {
     return <Loader size={'large'} color={colors.primary} />;
   }
-
   return (
     <ScrollView style={styles.container}>
       <Carousel data={carouselData} />
-      <HorizontalMovieScroll data={popularData} sectionTitle="Popular movies" />
-      <HorizontalMovieScroll data={mostViewedData} sectionTitle="Most viewed" />
-      <HorizontalMovieScroll data={oldData} sectionTitle="Old is Gold" />
+      <HorizontalMovieScroll
+        url={discover}
+        sortBy="popularity.desc"
+        sectionTitle="Popular movies"
+      />
+      <HorizontalMovieScroll
+        url={discover}
+        sortBy="vote_count.desc"
+        sectionTitle="Most viewed"
+      />
+      <HorizontalMovieScroll
+        url={discover}
+        sortBy="primary_release_date.asc"
+        sectionTitle="Old is Gold"
+      />
     </ScrollView>
   );
 };
-
 export default Movies;
--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,81 +1,34 @@
-import {Text, TextInput, View, FlatList, ActivityIndicator} from 'react-native';
 import {useEffect, useState} from 'react';
+import {FlatList, Text, View, RefreshControl} from 'react-native';
 
-import MovieCard from '@components/movieCard';
 import Loader from '@components/loader';
+import MovieCard from '@components/movieCard';
 import SearchInput from '@components/searchInput';
-import {fetchData} from '@network/apiMethods';
-import {colors} from '@theme/themes';
 
+import usePagination from '@hooks/usePagination';
+import {search} from '@network/apiUrls';
+import {colors} from '@theme/themes';
 import {styles} from './styles';
 
 const Search = () => {
-  const [isLoading, setIsLoading] = useState(false);
+  
   const [searchTerm, setSearchTerm] = useState('');
-  const [isDataLoading, setIsDataLoading] = useState(false);
-  const [data, setData] = useState<MovieCardDataArr>([]);
-  const [page, setPage] = useState(1);
-
-  const loadMoreMovies = async () => {
-    if (isDataLoading) return;
-    setIsDataLoading(true);
-
-    const newMovies = await fetchData(`/search/movie`, {
-      params: {query: searchTerm, page: page + 1},
-    });
-
-    // Combine the current data with new data, ensuring no duplicates
-    setData(prevData => {
-      const existingIds = prevData.map(movie => movie.id);
-      const uniqueNewMovies = newMovies?.results?.filter(
-        ({id}: {id: number}) => !existingIds?.includes(id),
-      );
-
-      return [...prevData, ...uniqueNewMovies];
-    });
-
-    setPage(prevPage => prevPage + 1);
-    setIsDataLoading(false);
-  };
-
-  const handleEndReached = () => {
-    loadMoreMovies();
-  };
-
-  const getData = async () => {
-    setIsLoading(true);
-
-    const searchedMovies = await fetchData(`/search/movie`, {
-      params: {query: searchTerm, page: '1'},
-    });
-
-    const searchedData =
-      searchedMovies?.results?.map(
-        ({
-          id,
-          title,
-          poster_path,
-          overview,
-          vote_average,
-          vote_count,
-          release_date,
-        }: MovieCardData) => ({
-          id,
-          title,
-          poster_path,
-          overview,
-          vote_average,
-          vote_count,
-          release_date,
-        }),
-      ) || [];
-
-    setData(searchedData);
-    setIsLoading(false);
-  };
+  const [url, setUrl] = useState(search);
+  const {
+    data,
+    refreshing,
+    loadingMore,
+    handleRefresh,
+    loadMore,
+    initialLoader,
+  } = usePagination(url);
 
   useEffect(() => {
-    getData();
+    // Reset pagination state when search term changes
+    if (searchTerm) {
+      setUrl(`/search/movie?query=${encodeURIComponent(searchTerm)}`);
+      handleRefresh();
+    }
   }, [searchTerm]);
 
   return (
@@ -86,9 +39,9 @@ const Search = () => {
         placeHolder="Search movies by name..."
       />
 
-      {isLoading ? (
+      {initialLoader ? (
         <Loader />
-      ) : searchTerm != '' && !data.length ? (
+      ) : searchTerm && !data.length ? (
         <Text style={styles.tooltip}>No movie found</Text>
       ) : (
         <FlatList
@@ -96,10 +49,13 @@ const Search = () => {
           renderItem={({item}) => <MovieCard data={item} />}
           numColumns={2}
           contentContainerStyle={styles.contentContainerStyle}
-          onEndReached={handleEndReached}
-          onEndReachedThreshold={0.5}
+          onEndReached={loadMore}
+          onEndReachedThreshold={0.1}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />
+          }
           ListFooterComponent={
-            isDataLoading ? (
+            loadingMore ? (
               <Loader size={'small'} color={colors.primary} />
             ) : null
           }
