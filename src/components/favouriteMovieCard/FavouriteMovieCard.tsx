bef993b5365f9330c81a91da6c15d8c01f6a79bc
UtsavK55 <utsav@gkmit.co>
2024-10-29 22:22:51 +0530

    fix:resolve comments

--- src/components/favouriteMovieCard/FavouriteMovieCard.tsx
+++ src/components/favouriteMovieCard/FavouriteMovieCard.tsx
@@ -3,49 +3,53 @@ import {useNavigation} from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather';
 
 import Rating from '@components/rating';
-import {colors} from '@theme/themes';
+import {useThemeColors} from '@theme/themes';
 import {ROUTES} from '@constants/Routes';
-import {imageUrl} from '@helpers/helper';
+import {getImageUrl} from '@helpers/helper';
 
 import {styles} from './styles';
 
 const FavouriteMovieCard = ({movie}: {movie: MovieCardData}) => {
   const navigation = useNavigation<MovieNavigationType>();
   const {id, title, poster_path, vote_average, vote_count, overview} = movie;
+  const colors = useThemeColors();
+
+  const handleMoviePress = () => {
+    navigation.navigate(ROUTES.MOVIES_STACK_SCREEN.MOVIE_DETAILS, {
+      movieId: id,
+    });
+  };
+
+  const handleRemovePress = () => {
+    navigation.navigate(ROUTES.MOVIES_STACK_SCREEN.MODAL_SCREEN, {
+      movieId: id,
+      title: title,
+      isAdding: false,
+    });
+  };
 
   return (
-    <TouchableOpacity
-      onPress={() =>
-        navigation.navigate(ROUTES.MOVIES_STACK_SCREEN.MOVIE_DETAILS, {
-          movieId: id,
-        })
-      }>
-      <View style={styles.movieItem}>
+    <TouchableOpacity onPress={handleMoviePress}>
+      <View style={styles().movieItem}>
         <Image
-          source={{uri: imageUrl(poster_path)}}
-          style={styles.movieImage}
+          source={{uri: getImageUrl(poster_path)}}
+          style={styles().movieImage}
         />
-        <View style={styles.movieInfo}>
-          <Text style={styles.movieTitle} numberOfLines={1}>
+        <View style={styles().movieInfo}>
+          <Text style={styles().movieTitle} numberOfLines={1}>
             {title}
           </Text>
-          <View style={styles.ratingContainer}>
+          <View style={styles().ratingContainer}>
             <Rating vote_average={vote_average} />
-            <Text style={styles.info}>({vote_count})</Text>
+            <Text style={styles().info}>({vote_count})</Text>
           </View>
-          <Text style={styles.info} numberOfLines={3}>
+          <Text style={styles().info} numberOfLines={3}>
             {overview}
           </Text>
         </View>
         <TouchableOpacity
-          onPress={() =>
-            navigation.navigate(ROUTES.MOVIES_STACK_SCREEN.MODAL_SCREEN, {
-              movieId: id,
-              title: title,
-              isAdding: false,
-            })
-          }
-          style={styles.removeButton}>
+          onPress={handleRemovePress}
+          style={styles().removeButton}>
           <Icon name="trash" size={20} color={colors.danger} />
         </TouchableOpacity>
       </View>
--- src/components/favouriteMovieCard/styles.ts
+++ src/components/favouriteMovieCard/styles.ts
@@ -1,54 +1,58 @@
-import {colors} from '@theme/themes';
 import {StyleSheet} from 'react-native';
+import {spacing, useThemeColors} from '@theme/themes';
 
-export const styles = StyleSheet.create({
-  
-  movieItem: {
-    flexDirection: 'row',
-    backgroundColor: colors.background,
-    borderRadius: 10,
-    marginBottom: 12,
-    padding: 12,
-    shadowColor: colors.foreground,
-    shadowOffset: {width: 0, height: 1},
-    shadowOpacity: 0.2,
-    shadowRadius: 1,
-    elevation: 2,
-    position: 'relative',
-  },
-  movieImage: {
-    width: 80,
-    height: 120,
-    borderRadius: 8,
-    marginRight: 12,
-  },
-  movieInfo: {
-    flex: 1,
-    gap: 10,
-    marginRight: 10,
-  },
-  movieTitle: {
-    fontSize: 22,
-    fontWeight: '600',
-    marginBottom: 4,
-    marginRight: 10,
-  },
-  ratingContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  removeButton: {
-    position: 'absolute',
-    right: 10,
-    top: 14,
-  },
-  info: {
-    color: colors.textSecondary,
-    fontWeight: 'bold',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-});
+export const styles = () => {
+  const colors = useThemeColors();
+
+  return StyleSheet.create({
+    movieItem: {
+      flexDirection: 'row',
+      backgroundColor: colors.background,
+      borderRadius: 10,
+      marginBottom: 12,
+      padding: 12,
+      shadowColor: colors.foreground,
+      shadowOffset: {width: 0, height: 1},
+      shadowOpacity: 0.2,
+      shadowRadius: 1,
+      elevation: 2,
+      position: 'relative',
+    },
+    movieImage: {
+      width: 80,
+      height: 120,
+      borderRadius: 8,
+      marginRight: 12,
+    },
+    movieInfo: {
+      flex: 1,
+      gap: 10,
+      marginRight: 10,
+    },
+    movieTitle: {
+      fontSize: spacing.l,
+      fontWeight: '600',
+      marginBottom: 4,
+      marginRight: 10,
+      color: colors.foreground,
+    },
+    ratingContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    removeButton: {
+      position: 'absolute',
+      right: 10,
+      top: 14,
+    },
+    info: {
+      color: colors.textSecondary,
+      fontWeight: 'bold',
+    },
+    titleContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    },
+  });
+};
